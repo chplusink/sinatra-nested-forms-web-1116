15f7474ffec28c1ca21dfab815997b312f3cb537
Vic Lam <lam.vic.k@gmail.com>
2016-12-19 17:54:02 -0500

    cappy!

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  ALL = []
+
+  def initialize(attributes)
+    attributes.each do |key,value|
+      self.send("#{key}=",value)
+    end
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL = []
+
+  def initialize(attributes)
+    attributes.each do |key,value|
+      self.send("#{key}=",value)
+    end
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    ALL.clear
+  end
+
 end
